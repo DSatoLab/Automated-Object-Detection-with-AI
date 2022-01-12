b96258668e22af00003606fc5ea6dba6b59389ad
Yiran Wu <32823396+kevin666aa@users.noreply.github.com>
2022-01-12 23:14:20 +0000

    Update kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -38,6 +38,8 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 def gray2rgb(image):
+    ''' Add channels to grayscale, used in plot_result.
+    '''
     width, height = image.shape
     out = np.empty((width, height, 3), dtype=np.uint8)
     out[:, :, 0] = image
@@ -46,12 +48,19 @@ def gray2rgb(image):
     return out
 
 def plot_result(img:np.ndarray, mask:np.ndarray, fig_path:str, name_panel:str, name_single:str) -> None:
+    ''' plot results with four panels.
+    Params:
+        img: the background image or the original image.
+        mask: the mask generated, should have the same shape as img.
+        fig_path: path to save the figures
+        name_panel: name of the four panel plot, set to '' to avoid saving the fig
+        name_single: name of the single red mask image to be saved, set to '' to avoid saving the fig
+    '''
 #     img=mpimg.imread(im_path)
     fig = plt.subplots(nrows=2, ncols=2, figsize=(10,10))
     
     ax1 = plt.subplot(2, 2, 1)
     imgplot = plt.imshow(img, cmap="gray")
-
 
     ax2 = plt.subplot(2, 2, 2)
     imgplot = plt.imshow(mask, cmap="gray")
@@ -84,10 +93,18 @@ def plot_result(img:np.ndarray, mask:np.ndarray, fig_path:str, name_panel:str, n
     if name_panel != '':
         plt.savefig(os.path.join(fig_path, name_panel))
 #     plt.show()
-
     plt.clf()
     
+
 def k_means(data, loop=50):
+    ''' The k_means function that does only binary classification.
+    
+    Params: 
+        data: in shap of [h, w, frames]
+        loop: num of iterations for k-means.
+        
+     
+    '''
     _,_,dim = data.shape
     c1 = np.ones([dim,1])
     c2 = np.zeros([dim,1])
@@ -96,19 +113,20 @@ def k_means(data, loop=50):
     oldss = ss
 
     for i in range(loop):
-    #     print(i)
+        # calculate distances
         c1 = np.array([np.repeat(c1[i], 10000).reshape(100,100) for i in range(dim)])
         c1 = np.moveaxis(c1, 0, -1)
 
         c2 = np.array([np.repeat(c2[i], 10000).reshape(100,100) for i in range(dim)])
         c2 = np.moveaxis(c2, 0, -1)
-
+        
         d1 = np.sum((data-c1)**2, 2)
         d2 = np.sum((data-c2)**2, 2)
 
         ss[d1<d2] = 1
         ss[d1>d2] = 2
 
+        # reassign center point 1
         if len(ss[ss==1]) > 0:
             tmp = data
             xxx = np.ones([100,100])
@@ -120,7 +138,7 @@ def k_means(data, loop=50):
         else:
             c1 = np.random.rand(dim,1)
 
-
+        # reassign center point 2
         if len(ss[ss==2]) > 0:
             tmp = data
             xxx = np.ones([100,100])
@@ -134,6 +152,7 @@ def k_means(data, loop=50):
 
 def over_thresh(mask, r:int, i, j, thresh):
     '''Help function for remove_noise function.
+    Check if the counted points go over the threshold.
     '''
     count = -1
     v = mask[i][j]
@@ -147,6 +166,19 @@ def over_thresh(mask, r:int, i, j, thresh):
 
 def remove_noise(mask:np.ndarray, thresh = 1, exten = 1, rheart=True, rnonheart=True):
     '''To remove noises from the mask.
+    
+    Params:
+        mask: two dimension array filled with only 1s and 0s.
+        thresh: threshold of the points.
+        exten: the kernel size, or the extension from a point. 
+                If set to 1, the center point would have 1 pixel extend to up, down, left and right.
+                So this makes the kernel 3x3 square, with 9 pixels.
+        rheart: whether to change/remove 1s. If set to false, the over_thresh function would not be applied to pixels that have a value 1.
+                So all 1s would not be changed.
+        rnonheart: whether to remove 0s.
+    
+    Returns:
+        mask: the refined mask.
     '''
     s1 = mask.shape[0]
     s2 = mask.shape[1]
@@ -177,19 +209,21 @@ args = argparser()
 name = args.n
 fr = int(args.f)
 
+# data is prepared as .mat files here
+# data should be in shape of [h, w, total_frames] here, representing n grayscale images of n frames.
 print(name + ' with ' + str(fr) + ' frame')
 dt = sio.loadmat('../data/' + name + '.mat')
 data = dt['images1'].astype(np.double)
 
+# normalization
 img = data[:,:,0]
 img = (img - np.min(img)) / (np.max(img) - np.min(img)) *255 
-# Image.fromarray((img).astype(np.uint8)).save(os.path.join('../plots', name+'origin.jpg')) # save single original
 
 dmin = np.repeat(np.min(data, 2)[:, :, np.newaxis], 1024, axis=2)
 dmax = np.repeat(np.max(data, 2)[:, :, np.newaxis], 1024, axis=2)
-data = (data - dmin) / (dmax - dmin) # minmax normalization
+data = (data - dmin) / (dmax - dmin) 
 
-
+# slice data to use intended frames
 data = data[:,:,:fr] # slice frame 
 print(data.shape)
 ss = k_means(data, 50)
@@ -200,22 +234,6 @@ ss = np.where(ss == 2, 1, 0)
 print(f'Heart Ratio: {str((1 - np.count_nonzero(ss) / 10000))}')
 plot_result(img, ss, '../outputs/',  name+"KMeansOrigin"+str(fr), name+'kmeans'+str(fr)+'_orginal.png')
 
-from scipy.ndimage import median_filter
-import cv2
-import numpy as np
-from matplotlib import pyplot as plt
-
-mf = copy.deepcopy(ss)
-p  = median_filter(mf ,(3,3)) # [[1,0,0],[1,0,1]]
-
-tmp = mf + p 
-tmp = np.where(tmp==1, 1, 0)
-print(np.count_nonzero(tmp))
-
-# if mask is reversed, change here to ss = np.where(ss == 2, 0, 1)
-plot_result(img, p, '../outputs/',  name+"KMeansMedian"+str(fr), name+'kmeans'+str(fr)+'_median.png')
-
-
 kk = copy.deepcopy(ss)
 kk = remove_noise(kk, 20, 3)
 kk = remove_noise(kk, 10, 2)
@@ -224,12 +242,27 @@ tmp = kk + ss
 tmp = np.where(tmp==1, 1, 0)
 print(np.count_nonzero(tmp))
 
+# if mask is reversed, change here to ss = np.where(ss == 2, 0, 1)
 plot_result(img, kk, '../outputs/', name+"KMeansPostp"+str(fr), name+'kmeans' + str(fr) +'_postprocess.png')
 
 
+# addtional 1: compare with median_filter
+# from scipy.ndimage import median_filter
+# import cv2
 
+# mf = copy.deepcopy(ss)
+# p  = median_filter(mf ,(3,3)) # [[1,0,0],[1,0,1]]
+
+# tmp = mf + p 
+# tmp = np.where(tmp==1, 1, 0)
+# print(np.count_nonzero(tmp))
+
+# # if mask is reversed, change here to ss = np.where(ss == 2, 0, 1)
+# plot_result(img, p, '../outputs/',  name+"KMeansMedian"+str(fr), name+'kmeans'+str(fr)+'_median.png')
+
+
+# addtional 2: using different start and end frames
 # def slice_prdict(data, start, end, name_single):
-    
 #     d = data[:,:, start:end]
 #     ss = k_means(d, 50)
 #     # if mask is reversed, change here to ss = np.where(ss == 2, 0, 1)
@@ -238,8 +271,6 @@ plot_result(img, kk, '../outputs/', name+"KMeansPostp"+str(fr), name+'kmeans' + 
 #         ss = np.where(ss == 1, 0, 1)
 #     print(f'Heart Ratio: {str((np.count_nonzero(ss) / 10000))}')
 #     plot_result(img, ss, '../outputs/',  '', name_single)
-
-    
 # slice_prdict(data, 0, 30, '1kmeans_orginal.png')
 # slice_prdict(data, 30, 60, '2kmeans_orginal.png')
 # slice_prdict(data, 60, 90, '3kmeans_orginal.png')
